849c460235a29af67229c83700f6b564353c85ad
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-02-09 11:17:01 +0000

    Update day5.sql

--- mySQL/Day5/Day5.sql
+++ mySQL/Day5/Day5.sql
@@ -27,6 +27,13 @@ select count(*) as 'Total No. of Emp' from employee;
 -- display max and min salary that you pay to your employee
 SELECT MAX(salary) AS 'max_salary', MIN(salary) AS 'min_salary'
 FROM employee;
+
 -- display max and min salary only for male employees
 SELECT MAX(salary) AS 'max_salary', MIN(salary) AS 'min_salary'
 FROM employee where SEX='M';
+
+-- Grouping Data
+-- GROUP BY - groups rows that have the same values into summary rows
+
+-- Display how many male and how many female employees are working in your company?
+select sex, count(*) as 'No of Emp' from employee where sex is not null group by sex;
